0f6fa979fb2fe994cb774b01e66e01c4a70231a8
RustedBird <vlasov.31189@gmail.com>
2018-07-11 20:04:37 +0300

    init commit

---
+++ lesson1/index.php
@@ -0,0 +1,86 @@
+<?php
+
+/*const MY_CONST = 'Some Value';
+define('FOO', 'что-то');
+
+echo 'lesson 1';
+
+$a = 1;
+$b = 1.1;
+$c = 'text';
+$array = [1, 2, 3, 'string' => 'text'];
+$e = true;
+
+$str = 'This is isn\'t string';
+
+unset($a);
+
+$name = 'Philipp';
+echo '<p>Привет, ' . $name . '</p>';
+echo "<p>Привет, $name</p>";
+echo "<p>Привет, {$name}s</p>";
+echo $str;
+
+echo '<pre>';
+
+echo $a;
+
+
+var_dump($a, $b, $c, $array, $e);
+echo $array[0] . '<br>';
+echo $array['string'];
+
+echo '</pre>';
+
+$a = 4;
+$b = 2;
+
+echo 'zxcxzc ' . ($a += 2) . '<br>';
+
+echo 'Sum ' . ($a + $b) . '<br>';
+echo $a - $b . '<br>';
+echo $a / $b . '<br>';
+echo $a % $b . '<br>';
+echo 'Степень ' . ($a ** $b) . '<br>';
+
+echo MY_CONST . '<br>';
+echo FOO . '<br>';
+
+$day = 8;
+
+if (!($day % 2)) {
+    echo 'Good';
+} else {
+    echo 'Bad';
+}*/
+
+/*$i = 2;
+
+switch (true) {
+    case $i <= 0:
+        echo 'i <= 0';
+        break;
+    case $i >= 0:
+        echo 'i >= 0';
+        break;
+}*/
+
+/*$date = 2;
+$dayName = 'monday';
+
+if ($date === 16 && $dayName == 'monday') {
+    echo '<pre>';
+    echo 'Сегодня английский';
+    echo '</pre>';
+} else {
+    echo '<pre>';
+    echo 'Английский будет не сегодня(';
+    echo '</pre>';
+}*/
+
+$array = [1, 'text', 2];
+//echo $array;
+
+$name = 'Philipp';
+$fullName = "$name Vlasov";
+echo $fullName;
---
+++ lesson2/index.php
@@ -0,0 +1,52 @@
+<?php
+
+$array = [1, 2, 'name' => 'Philipp', 4, 5, 6];
+
+for ($i = 0; $i < count($array); $i++) {
+    echo '<pre>';
+    echo $array[$i] . '<br>';
+    echo '</pre>';
+}
+
+foreach ($array as $key => $value) {
+    if ($key === 0) {
+        echo '<pre>';
+        echo  "Key: $key, Value: $value";
+        echo '</pre>';
+        break;
+    }
+}
+$i = 10;
+while ($i < 10) {
+    echo $i . '<br>';
+    $i++;
+}
+
+do {
+    echo "i = $i <br>";
+    $i++;
+} while ($i < 10);
+echo '<hr>';
+
+
+$a = 5;
+$b = 2;
+
+/**
+ * Function for placing number into some degree
+ *
+ * @param int $num
+ * @param int $degree
+ * @return int
+ */
+function myPow (int $num, $degree = 2) {
+    $result = $num ** $degree;
+    return $result;
+}
+
+echo myPow(3);
+
+
+
+
+
